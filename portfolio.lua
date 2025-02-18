7b0065150b5cc7a3b7309fc36ae24b1657fb5e5c
jandermoreira <moreira.jander@gmail.com>
2025-02-18 18:53:41 -0300

    Atualiza imagens e links

--- portfolio.lua
+++ portfolio.lua
@@ -1,25 +1,39 @@
 -- portfolio.lua
 
+function www(link)
+    quarto.log.output(link.target)
+    if link.content[1] and pandoc.utils.stringify(link.content[1]) == "++www" then
+        link.content[1] = pandoc.Image(
+            {},
+            "/globo.png",
+            link.target,
+            { target = "_blank" }
+        )
+    end
+    return link
+end
+
 function linking(word)
     local element = word
     if string.sub(word.text, 1, 5) == "++gh:" then
         element = pandoc.Link(
             pandoc.Image({}, "/github.png", "", {}),
             "https://github.com/jandermoreira/" .. string.sub(word.text, 6),
-            "GitHub",
-            { target = "_blank"}
+            "GitHub: " .. string.sub(word.text, 6),
+            { target = "_blank" }
         )
     elseif string.sub(word.text, 1, 7) == "++ctan:" then
         element = pandoc.Link(
             pandoc.Image({}, "/ctan.png", "", {}),
             "https://ctan.org/pkg/" .. string.sub(word.text, 8),
-            "CTAN",
-            { target = "_blank"}
+            "CTAN: " .. string.sub(word.text, 8),
+            { target = "_blank" }
         )
-    end 
+    end
     return element
 end
 
 return {
-    {Str = linking}
+    { Str = linking },
+    { Link = www },
 }
